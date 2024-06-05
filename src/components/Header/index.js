95f0ab747cce2f9d7d21fe84e6091103a1d7017a
Sivaranjan K <146953486+Sk-Siva@users.noreply.github.com>
2024-06-05 16:33:06 +0530

    Update Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,34 +15,30 @@ const Header = props => {
   }
 
   return (
-    <ul className="nav-list-con">
-      <div className="header-con">
-        <Link to="/">
+    <ul className='nav-list-con'>
+      <div className='header-con'>
+        <Link to='/'>
           <li>
             <img
-              className="logo"
-              src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
-              alt="website logo"
+              className='logo'
+              src='https://assets.ccbp.in/frontend/react-js/logo-img.png'
+              alt='website logo'
             />
           </li>
         </Link>
         <nav>
-          <ul className="nav-list-con">
-            <li className="nav-item">
-              <Link to="/" className="link">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/jobs" className="link">
-                Jobs
-              </Link>
-            </li>
+          <ul className='nav-list-con'>
+            <Link to='/' className='link'>
+              <li className='nav-item'>Home</li>
+            </Link>
+            <Link to='/jobs' className='link'>
+              <li className='nav-item'>Jobs</li>
+            </Link>
           </ul>
         </nav>
 
         <li>
-          <button type="button" className="login-btn" onClick={onLogout}>
+          <button type='button' className='login-btn' onClick={onLogout}>
             Logout
           </button>
         </li>
